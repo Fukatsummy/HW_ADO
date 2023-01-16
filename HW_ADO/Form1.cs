63c9f47e85c7ca2ee03a357c5a5b916c434c693c
PS <PS@DESKTOP-T711T4O>
2023-01-16 22:01:11 +0300

    Add project files.

---
+++ HW_ADO/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW_ADO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection connection = null;
+            string connectionString = @"Data Source = (localdb)\MSSQLLocalDB; 
+            Initial Catalog = Warehouse; 
+            Integrated Security = True; ";
+            connection = new SqlConnection("");
+            connection.ConnectionString = connectionString;
+            connection.Open();
+            MessageBox.Show("123");
+            //connection.ConnectionString = connectionString;
+            connection.Close();
+            MessageBox.Show("321");
+            /*Connect Timeout = 30; 
+            Encrypt = False;
+            TrustServerCertificate = False;
+            ApplicationIntent = ReadWrite;
+            MultiSubnetFailover = False*/
+        }
+    }
+}
